9f74f04693f966bccd1a5903f829657d6df219ee
nicolassims <sableye.nico@gmail.com>
2021-03-04 12:52:34 -0800

    Added more accessors.

--- lib/events_app/users/user.ex
+++ lib/events_app/users/user.ex
@@ -10,6 +10,14 @@ defmodule EventsApp.Users.User do
     timestamps()
   end
 
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user_by_name(name) do
+    Repo.get_by(User, name: name)
+  end
+
   @doc false
   def changeset(user, attrs) do
     user
