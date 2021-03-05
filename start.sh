61351ec522cdcde1c46544f204124b6b4a579b98
nicolassims <sableye.nico@gmail.com>
2021-03-04 19:59:09 -0800

    Add deploy files.

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod
+export PORT=4900
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/multibulls/bin/multibulls stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/multibulls/bin/multibulls start
